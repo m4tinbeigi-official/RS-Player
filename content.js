a49e28883df9a5b03475b71b1f086e8fb93369cf
Rick Sanchez <matinbeigi99@gmail.com>
2024-10-01 09:04:35 +0330

    add icons

--- content.js
+++ content.js
@@ -2,24 +2,32 @@ document.querySelectorAll('a').forEach(function (link) {
   const href = link.getAttribute('href');
 
   if (href && href.match(/\.(mp4|mkv|avi|mov)$/i)) {
-    // ایجاد دکمه VLC
-    let vlcButton = document.createElement('button');
-    vlcButton.innerText = 'Play in VLC';
-    vlcButton.style.margin = '0 5px';
-    vlcButton.onclick = function () {
+    // ایجاد آیکون VLC
+    let vlcIcon = document.createElement('img');
+    vlcIcon.src = chrome.runtime.getURL('icons/vlc.png'); // آدرس آیکون VLC
+    vlcIcon.alt = 'Play in VLC';
+    vlcIcon.style.width = '20px';
+    vlcIcon.style.height = '20px';
+    vlcIcon.style.margin = '0 5px';
+    vlcIcon.style.cursor = 'pointer';
+    vlcIcon.onclick = function () {
       window.location.href = 'vlc://' + href;
     };
 
-    // ایجاد دکمه PotPlayer
-    let potButton = document.createElement('button');
-    potButton.innerText = 'Play in PotPlayer';
-    potButton.style.margin = '0 5px';
-    potButton.onclick = function () {
+    // ایجاد آیکون PotPlayer
+    let potIcon = document.createElement('img');
+    potIcon.src = chrome.runtime.getURL('icons/potplayer.png'); // آدرس آیکون PotPlayer
+    potIcon.alt = 'Play in PotPlayer';
+    potIcon.style.width = '20px';
+    potIcon.style.height = '20px';
+    potIcon.style.margin = '0 5px';
+    potIcon.style.cursor = 'pointer';
+    potIcon.onclick = function () {
       window.location.href = 'potplayer://' + href;
     };
 
-    // اضافه کردن دکمه‌ها کنار لینک دانلود
-    link.parentNode.insertBefore(vlcButton, link.nextSibling);
-    link.parentNode.insertBefore(potButton, vlcButton.nextSibling);
+    // اضافه کردن آیکون‌ها کنار لینک دانلود
+    link.parentNode.insertBefore(vlcIcon, link.nextSibling);
+    link.parentNode.insertBefore(potIcon, vlcIcon.nextSibling);
   }
 });
